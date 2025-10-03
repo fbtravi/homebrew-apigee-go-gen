851ae486d336edd52a8a61e1bbc314058ea613ee
Felipe Travi <felipe.travi@g.globo>
2025-10-03 17:46:15 -0300

    Fix apigee-go-gen version

--- Formula/apigee-go-gen.rb
+++ Formula/apigee-go-gen.rb
@@ -1,14 +1,12 @@
 class ApigeeGoGen < Formula
   desc "Tool to generate Apigee proxies, flows etc in Go"
   homepage "https://apigee.github.io/apigee-go-gen/"
-  url "https://github.com/apigee/apigee-go-gen/archive/refs/tags/v0.1.13.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  url "https://github.com/apigee/apigee-go-gen/releases/download/v1.1.0-beta.2/apigee-go-gen_Darwin_arm64.tar.gz"
+  sha256 "0b6d6dd69340109336a81dae1784ea5f4ac6fc731ed08bf3628b1d0c3264b328"
   license "Apache-2.0"
 
-  depends_on "go" => :build
-
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/apigee-go-gen"
+    bin.install "apigee-go-gen"
   end
 
   test do
