5d6cdf78784d3e6c7d70d76f1e8c6805933a71bc
Felipe Travi <felipe.travi@g.globo>
2025-10-04 15:18:30 -0300

    feat: add multi-platform support for ApigeeGoGen formula

--- Formula/apigee-go-gen.rb
+++ Formula/apigee-go-gen.rb
@@ -1,9 +1,26 @@
 class ApigeeGoGen < Formula
   desc "Tool to generate Apigee proxies, flows etc in Go"
   homepage "https://apigee.github.io/apigee-go-gen/"
-  url "https://github.com/apigee/apigee-go-gen/releases/download/v1.1.0-beta.2/apigee-go-gen_Darwin_arm64.tar.gz"
-  sha256 "0b6d6dd69340109336a81dae1784ea5f4ac6fc731ed08bf3628b1d0c3264b328"
   license "Apache-2.0"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/apigee/apigee-go-gen/releases/download/v1.1.0-beta.2/apigee-go-gen_Darwin_arm64.tar.gz"
+      sha256 "0b6d6dd69340109336a81dae1784ea5f4ac6fc731ed08bf3628b1d0c3264b328"
+    else
+      url "https://github.com/apigee/apigee-go-gen/releases/download/v1.1.0-beta.2/apigee-go-gen_Darwin_x86_64.tar.gz"
+      sha256 "c385138135abadf9807a396b3bd5d9a36d4e2a47dd5a9dcbad68d02a8a7a27e8"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      odie "ApigeeGoGen binary not available for Linux ARM64 yet"
+    else
+      url "https://github.com/apigee/apigee-go-gen/releases/download/v1.1.0-beta.2/apigee-go-gen_Linux_x86_64.tar.gz"
+      sha256 "86d06b54b8c2d516cce29decb1c3f68f5615a7e7b47ad49587807fdad0f018e6"
+    end
+  end
 
   def install
     bin.install "apigee-go-gen"
